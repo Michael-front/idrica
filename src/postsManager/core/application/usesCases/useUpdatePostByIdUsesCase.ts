6046bfd1403b88d6aea5adac07a63c27dfb734fc
Michael Coloma Calva <neomichael87@gmail.com>
2024-08-05 22:39:41 +0200

    Update to rename const to updatePostById

--- src/postsManager/core/application/usesCases/useUpdatePostByIdUsesCase.ts
+++ src/postsManager/core/application/usesCases/useUpdatePostByIdUsesCase.ts
@@ -3,6 +3,6 @@ import { IPostsManagerApiPort } from "../../domain/ports/PostsManagerApiPort";
 import { useUpdatePostByIdAdapter } from "src/postsManager/infrastructure/api/adapters/useUpdatePostByIdAdapter";
 
 export const useUpdatePostByIdUseCase = (): IPostsManagerApiPort["useUpdatePostById"] => {
-  const UpdatePostById = useUpdatePostByIdAdapter();
-  return (params: Partial<ApiResponsePost>) => UpdatePostById(params);
+  const updatePostById = useUpdatePostByIdAdapter();
+  return (params: Partial<ApiResponsePost>) => updatePostById(params);
 };
