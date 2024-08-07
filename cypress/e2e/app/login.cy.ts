cef2c52a41e99ff0d3b022dda87beb989ae3591a
Michael Coloma Calva <neomichael87@gmail.com>
2024-08-07 10:01:21 +0200

    Update to export functio to do login

--- cypress/e2e/app/login.cy.ts
+++ cypress/e2e/app/login.cy.ts
@@ -1,3 +1,4 @@
+import { doLogin } from "support/util";
 import { ROUTES_PATH } from "../../../src/postsManager/config/routes/routes";
 
 describe("Login Page", () => {
@@ -6,11 +7,7 @@ describe("Login Page", () => {
   });
 
   it("should log in successfully with correct credentials via email", () => {
-    cy.get('[class*="login__input"]').first().type("Sincere@april.biz");
-
-    cy.get('[class*="login__input"]').last().type("1234");
-
-    cy.get('[data-testid="login-button"]').click();
+    doLogin("Sincere@april.biz", "1234");
 
     cy.url().should("include", ROUTES_PATH.HOME);
 
@@ -18,11 +15,7 @@ describe("Login Page", () => {
   });
 
   it("should log in successfully with correct credentials via username", () => {
-    cy.get('[class*="login__input"]').first().type("Bret");
-
-    cy.get('[class*="login__input"]').last().type("1234");
-
-    cy.get('[data-testid="login-button"]').click();
+    doLogin("Bret", "1234");
 
     cy.url().should("include", ROUTES_PATH.HOME);
 
@@ -30,11 +23,7 @@ describe("Login Page", () => {
   });
 
   it("should show error with incorrect credentials", () => {
-    cy.get('[class*="login__input"]').first().type("wrongUsername");
-
-    cy.get('[class*="login__input"]').last().type("wrongPassword");
-
-    cy.get('[data-testid="login-button"]').click();
+    doLogin("wrogUserName", "wrongPassword");
 
     cy.url().should("include", ROUTES_PATH.LOGIN);
   });
--- cypress/support/util.ts
+++ cypress/support/util.ts
@@ -4,3 +4,9 @@ export function checkHeader() {
   cy.get('[class*="header__menu-item"]').should("have.length", 1);
   cy.get('[class*="header__login"]').should("have.length", 1);
 }
+
+export const doLogin = (user: string, password: string) => {
+  cy.get('[class*="login__input"]').first().type(user);
+  cy.get('[class*="login__input"]').last().type(password);
+  cy.get('[data-testid="login-button"]').click();
+};
