685af152e8176ee20b7c83f35976910f9410230b
Michael Coloma Calva <neomichael87@gmail.com>
2024-08-07 09:09:17 +0200

    Update tsconfig and webpack  cypress to be similar to proyect

--- cypress/webpack.config.js
+++ cypress/webpack.config.js
@@ -1,19 +1,40 @@
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
+    alias: {
+      '@assets': path.resolve(__dirname, '../src/postsManager/adapters/primary/ui/assets'),
+      '@components': path.resolve(__dirname, '../src/postsManager/adapters/primary/ui/components')
+    },
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.module\.css$/,
         use: [
+          'style-loader',
           {
-            loader: 'ts-loader',
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+            },
           },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
       },
     ],
