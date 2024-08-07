74b1660f84de8953e806a8f9bdd2e9083595e433
Michael Coloma Calva <neomichael87@gmail.com>
2024-08-07 09:10:11 +0200

    Update to execute node (process.cwd) to get main path

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,14 @@
-import webpack from "@cypress/webpack-preprocessor";
+//import webpack from "@cypress/webpack-preprocessor";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("@cypress/webpack-preprocessor");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
-export default (on) => {
+module.exports = (on) => {
   const options = {
-    webpackOptions: require("../../webpack.config"),
+    // webpackOptions: require("../../webpack.config"),
+    // webpackOptions: require(path.resolve(__dirname, "../webpack.config.js")),
+    webpackOptions: require(path.resolve(process.cwd(), "webpack.config.js")),
   };
   on("file:preprocessor", webpack(options));
 };
